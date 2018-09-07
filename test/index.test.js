d84cfc1a604693d0ec8aae5a12f99941f7584505
DESKTOP-J7RM642\admin <397091336@qq.com>
2018-09-07 18:08:10 +0800

    optimize tests

--- test/index.test.js
+++ test/index.test.js
@@ -9,15 +9,7 @@ const options = [
 options.forEach(option => {
   describe('# make-config-example ' + option, () => {
     it('should succeed', () => {
-      return new Promise((resolve, reject) => {
-        childProcess.exec('node ../lib/index.js ' + option, { cwd: __dirname }, (err) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve()
-          }
-        })
-      })
+      childProcess.execSync('node ../lib/index.js ' + option, { cwd: __dirname })
     })
   })
 })
